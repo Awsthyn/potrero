36ffa2caff205c404be3c8957ffdfb2c0b64529e
Agustina Rojas <agustinarojas2214@gmail.com>
2020-09-10 14:37:46 -0300

    minicommit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../Reducers/index';
+import rootReducer from './reducer';
 import thunk from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
