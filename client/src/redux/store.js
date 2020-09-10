abf60419f403e32c5a69962bf4166bd52269d969
elefantenegro <elefantenegro64@gmail.com>
2020-09-10 18:51:52 -0300

    First Step

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,12 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from './reducer';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import reducer from "./reducer";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
