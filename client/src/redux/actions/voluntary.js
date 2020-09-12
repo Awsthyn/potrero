91bcd7380689ea66d8bba453a170aac09e0bb3f2
Elena Note <elebeatrizgonzalez@gmail.com>
2020-09-11 23:31:14 -0300

    Agrego configuración para que al crear un voluntario, se envíe mail de Bienvenida. En form se coloca prevent default

--- client/src/redux/actions/voluntary.js
+++ client/src/redux/actions/voluntary.js
@@ -6,9 +6,20 @@ export function postVoluntary(voluntary) {
 		return axios
 			.post(`http://localhost:3001/volunteers`, voluntary, {withCredentials: true})
 			.then(res => {
-				
-				dispatch({type: POST_VOLUNTARY, voluntary: res.data});
+				//dispatch({type: POST_VOLUNTARY, voluntary: res.data});
+				dispatch(postMailWelcome(res.data))
 			})
 			.catch(err => console.log(err));
 	};
+}
+
+export function postMailWelcome(voluntary){
+	return function (dispatch) {
+		return axios
+			.post(`http://localhost:3001/mailWelcomeRejection/mail`, voluntary , {withCredentials : true})
+			.then(res =>{
+				dispatch({type: POST_VOLUNTARY , voluntary: res.data});
+			})
+			.catch(err => console.log(err));
+	}
 }
