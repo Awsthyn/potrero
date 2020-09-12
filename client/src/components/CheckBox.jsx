10c553895d981134f955e9437f7459986de7f85f
Agustina Rojas <agustinarojas2214@gmail.com>
2020-09-12 17:38:46 -0300

    horarios formulario voluntarios

--- client/src/components/CheckBox.jsx
+++ client/src/components/CheckBox.jsx
@@ -5,32 +5,50 @@ export default class CheckBox extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			[this.props.dia]: [{de: 8, hasta: 17}],
+			[this.props.dia]: [{de: 12, hasta: 14}],
 		};
 		this.handleOnClick = this.handleOnClick.bind(this);
+		this.handleTime = this.handleTime.bind(this);
 	}
 
 	handleOnClick(e) {
 		e.preventDefault();
 		this.setState({
-			[this.props.dia]: [...this.state[this.props.dia], {de: 9, hasta: 18}],
+			[this.props.dia]: [...this.state[this.props.dia], {de: 12, hasta: 14}],
 		});
 	}
-	handleTime(type, idx) {
-		if (type === 'aumentarDe') {
-			this.setState(function () {
-				var newState = [this.props.dia].map((h, i) => {
-					if (i === idx) {
-						return;
-					} else {
-						return;
+	handleTime(type, idx, clase) {
+		// if (type === 'aumentarDe') {
+		this.setState(function (state) {
+			console.log(type);
+			var newState = state[this.props.dia].map((h, i) => {
+				console.log(h);
+				console.log(h[clase]);
+				if (i === idx) {
+					if (type === 'aumentar' && clase === 'de' && h[clase] >= 8 && h[clase] < 17) {
+						return (h[clase] = h[clase] + 0.5);
+					} else if (type === 'aumentar' && clase === 'hasta' && h[clase] < 18 && h[clase] >= 9) {
+						return (h[clase] = h[clase] + 0.5);
+					} else if (type === 'disminuir' && clase === 'de' && h[clase] <= 17 && h[clase] > 8) {
+						return (h[clase] = h[clase] - 0.5);
+					} else if (type === 'disminuir' && clase === 'hasta' && h[clase] <= 18 && h[clase] > 9) {
+						console.log(h[clase] - 0.5);
+						return (h[clase] = h[clase] - 0.5);
 					}
-				});
+				} else {
+					return h;
+				}
 			});
-		}
+			console.log(newState);
+			return {
+				newState,
+			};
+		});
+		// }
 	}
 	render() {
 		let dia = this.state[this.props.dia];
+		console.log(this.props);
 		return (
 			<div>
 				<input type="checkbox" name="moday" value="lunes" />
@@ -40,18 +58,6 @@ export default class CheckBox extends React.Component {
 				})}
 				<button onClick={e => this.handleOnClick(e)}> + </button>
 				<br></br>
-				{/* <input type="checkbox" id="martes" name="tuesday" value="martes" />
-				<label htmlFor="martes"> Martes </label>
-				<br></br>
-				<input type="checkbox" id="miercoles" name="wednesday" value="miercoles" />
-				<label htmlFor="miercoles"> Miércoles </label>
-				<br></br>
-				<input type="checkbox" id="jueves" name="thursday" value="jueves" />
-				<label htmlFor="jueves"> Jueves </label>
-				<br></br>
-				<input type="checkbox" id="viernes" name="friday" value="viernes" />
-				<label htmlFor="viernes"> Viernes </label>
-				<br></br> */}
 			</div>
 		);
 	}
--- client/src/components/Horarios.jsx
+++ client/src/components/Horarios.jsx
@@ -10,45 +10,45 @@ export default function Horarios({de, hasta, id, handleTime}) {
 		if (de < 10 && de.toString().length === 1) {
 			setHoraDe('0' + de + ':00');
 		} else if (de < 10 && de.toString().length > 1) {
-			setHoraDe('0' + de + ':30');
+			setHoraDe('0' + (de - 0.5) + ':30');
 		} else if (de >= 10 && de.toString().length === 2) {
 			setHoraDe(de + ':00');
 		} else if (de >= 10 && de.toString().length > 2) {
-			setHoraDe(de + ':30');
+			setHoraDe(de - 0.5 + ':30');
 		}
 	};
 	const renderTimeHasta = () => {
 		if (hasta < 10 && hasta.toString().length === 1) {
 			setHoraHasta('0' + hasta + ':00');
 		} else if (hasta < 10 && hasta.toString().length > 1) {
-			setHoraHasta('0' + hasta + ':30');
+			setHoraHasta('0' + (hasta - 0.5) + ':30');
 		} else if (hasta >= 10 && hasta.toString().length === 2) {
 			setHoraHasta(hasta + ':00');
 		} else if (hasta >= 10 && hasta.toString().length > 2) {
-			setHoraHasta(hasta + ':30');
+			setHoraHasta(hasta - 0.5 + ':30');
 		}
 	};
 
 	useEffect(() => {
 		renderTimeDe();
 		renderTimeHasta();
-	}, []);
+	}, [de, hasta]);
 
 	return (
 		<div>
 			{/* <label htmlFor="hora">Hora</label> */}
-			<IconButton aria-label="delete" onClick={() => handleTime('aumentarDe', id)}>
+			<IconButton aria-label="delete" onClick={() => handleTime('aumentar', id, 'de')}>
 				<span className="material-icons">expand_less</span>
 			</IconButton>
-			<IconButton aria-label="delete">
+			<IconButton aria-label="delete" onClick={() => handleTime('disminuir', id, 'de')}>
 				<span className="material-icons">expand_more</span>
 			</IconButton>
-			<span> de: {horaDe} </span>
-			<span> a {horaHasta}</span>
-			<IconButton aria-label="delete">
+			<span> de: {horaDe} hs</span>
+			<span> a {horaHasta} hs</span>
+			<IconButton aria-label="delete" onClick={() => handleTime('aumentar', id, 'hasta')}>
 				<span className="material-icons">expand_less</span>
 			</IconButton>
-			<IconButton aria-label="delete">
+			<IconButton aria-label="delete" onClick={() => handleTime('disminuir', id, 'hasta')}>
 				<span className="material-icons">expand_more</span>
 			</IconButton>
 			{/* <button>Hora</button>
