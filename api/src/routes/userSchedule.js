4ba1d27e940c821734248c672dc7e8a60429b28b
Franco Matus <fmatus188gmail.com>
2020-09-13 03:17:32 -0300

    ignorar commit

--- api/src/routes/userSchedule.js
+++ api/src/routes/userSchedule.js
@@ -6,4 +6,21 @@ const { UserSchedule } = require('../db.js');
 // TRAEMOS SEQUELIZE
 const Sequelize = require('sequelize');
 
+server.get('/', (req, res) => {
+    UserSchedule.findAll({
+        where: {
+            include:[{
+                model: User,
+                include: Student
+            }]
+        }
+    })
+    .then( allSchedulesOfUsers => {
+        res.json( allSchedulesOfUsers )
+    })
+    .catch( err => {
+        res.json( err )
+    })
+})
+
 module.exports = server;
