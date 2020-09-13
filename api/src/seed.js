b2fafa1d96d174cce65f6a1841a0834ba64ad97d
elefantenegro <elefantenegro64@gmail.com>
2020-09-13 03:35:30 -0300

    Seeding

--- api/src/seed.js
+++ api/src/seed.js
@@ -13,28 +13,54 @@ const initialVolunteers = [
     phone: "3794 65-9956",
     birthday: Date(),
     email: "elebeatrizgonzalez@gmail.com",
+    state: "acepted",
     linkedin: "https://www.linkedin.com/in/ele-gonzalez/",
-    adviser: false },
+    adviser: true },
     { 
     firstName: "Germán",
     lastName: "Moren",
     phone: "3442 67-0833",
     birthday: Date(),
+    state: "acepted",
     email: "ingenieriamg91@gmail.com",
     linkedin: "https://www.linkedin.com/in/germanmoren/",
-    adviser: false },
+    adviser: true },
+    { 
+    firstName: "Leandro",
+    lastName: "Alvarez",
+    phone: "341 5967027",
+    birthday: Date(),
+    state: "acepted",
+    email: "elefantenegro64@gmail.com",
+    linkedin: "https://www.linkedin.com/in/leandro-agustin-alvarez/",
+    adviser: true },
+
   ];
 
 const initialUsers = [
     {
-    email: "alfredoromano@romano-group.com",
+    email: "elefantenegro64@gmail.com",
     password: "1234",
-    roleId: 1
+    roleId: 1,
+    volunteerId: 4
+    },
+    {
+    email: "ingenieriamg91@gmail.com",
+    password: "1234",
+    roleId: 1,
+    volunteerId: 3
+    },
+
+    {
+    email: "elebeatrizgonzalez@gmail.com",
+    password: "1234",
+    roleId: 1,
+    volunteerId: 2
     },
     {
     email: "martinborchandt@soyhenry.com",
     password: "1234",
-    roleId: 1
+    roleId: 1,
     },
     
 ]
