aa17430dc2b9df1d3c7272c7be97046edac5052f
Agustina Rojas <agustinarojas2214@gmail.com>
2020-09-14 21:22:43 -0300

    ruta userSchedule

--- api/src/routes/userSchedule.js
+++ api/src/routes/userSchedule.js
@@ -23,4 +23,24 @@ server.get('/', (req, res) => {
     })
 })
 
+server.post('/', (req, res) => {
+    var dias = req.body.schedules.split("-");
+    var numero = 1;
+    var obj = {};
+        for (let i = 0; i < dias.length; i++) {
+        if (numero === 1) {
+            obj.startTime = dias[i];
+            numero = numero + 1;
+        } else if (numero === 2) {
+            obj.endTime = dias[i];
+            numero = numero + 1;
+        } else if (numero === 3) {
+            obj.nameWeekDay = dias[i];
+            obj.studentId = sc.id;
+            UserSchedule.create(obj);
+            numero = 1;
+        }
+    }
+})
+
 module.exports = server;
