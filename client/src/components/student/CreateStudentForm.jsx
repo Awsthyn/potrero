43dc119283ce4b4c4bf7c3e7f45f8e7067f5f1bc
Awsthyn <aguswagner008@gmail.com>
2020-09-16 16:56:37 -0300

    checkboxes de strenghts

--- client/src/components/student/CreateStudentForm.jsx
+++ client/src/components/student/CreateStudentForm.jsx
@@ -5,6 +5,7 @@ import { getSubjects } from '../../redux/actions/subject'
 import { postStudent } from '../../redux/actions/student';
 
 import SubjectCheckbox from "./SubjectCheckbox"
+import StrengthCheckbox from "./StrengthCheckbox";
 
 export class CreateStudentForm extends Component {
 	constructor(props) {
@@ -15,15 +16,18 @@ export class CreateStudentForm extends Component {
 			phone: null,
 			email: null,
 			tutor: null,
+			tutorEmail: null,
+			tutorPhone: null,
 			difficulty: false,
 			subjectsId: [],
             weakness: 'lorem ipsum',
             interests: "lorem ipsum",
-			strengths: 'lorem ipsum',
+			strengths: [],
 			motivations: 'lorem ipsum',
 		};
 		this.subjects = this.props.subjects
 		this.onCheckboxClicked = this.onCheckboxClicked.bind(this);
+		this.onStrengthCheckboxClicked = this.onStrengthCheckboxClicked.bind(this);
 		this.onChangeHandler = this.onChangeHandler.bind(this);
 		this.submitHandler = this.submitHandler.bind(this);
 	}
@@ -49,6 +53,18 @@ export class CreateStudentForm extends Component {
                 subjectsId: this.state.subjectsId.filter(s => s !== subject.id)
             })
         }
+	}
+
+	onStrengthCheckboxClicked(subject, isChecked) {
+        if(isChecked){
+            this.setState({
+                strengths: [...this.state.strengths, subject.id]
+            })
+        } else {
+            this.setState({
+                strengths: this.state.strengths.filter(s => s !== subject.id)
+            })
+        }
     }
 
     componentDidMount() {
@@ -62,7 +78,7 @@ export class CreateStudentForm extends Component {
 			<form className="mx-auto" style={{ width: '80vw' }} onSubmit={this.submitHandler}>
 				<div className="mb-n1 d-flex flex-row form-group">
 					<input
-						style={{width: "550px"}}
+						style={{width: "40vw"}}
 						className="form-control"
 						type="text"
 						name="firstName"
@@ -70,7 +86,7 @@ export class CreateStudentForm extends Component {
 						onChange={this.onChangeHandler}
 					/>
 					<input
-						style={{width: "550px"}}
+						style={{width: "40vw"}}
 						className="form-control"
 						type="text"
 						name="lastName"
@@ -78,25 +94,23 @@ export class CreateStudentForm extends Component {
 						onChange={this.onChangeHandler}
 					/>
 				</div>
-				<div className="form-group">
-					<input
+				<div className="mb-4 d-flex flex-row form-group">
+					<input style={{width: "40vw"}}
 						className="form-control"
 						type="text"
 						name="phone"
-						placeholder="Teléfono de contacto..."
+						placeholder="Teléfono del alumno..."
 						onChange={this.onChangeHandler}
 					/>
-				</div>
-				<div className="form-group">
-					<input
+					<input style={{width: "40vw"}}
 						className="form-control"
 						type="text"
 						name="email"
-						placeholder="Email de contacto..."
+						placeholder="Email del alumno..."
 						onChange={this.onChangeHandler}
 					/>
 				</div>
-				<div className="form-group">
+				<div className="mt-4 form-group">
 					<input
 						className="form-control"
 						type="text"
@@ -105,12 +119,28 @@ export class CreateStudentForm extends Component {
 						onChange={this.onChangeHandler}
 					/>
 				</div>
-				<div className="form-group">
-					<input
+				<div className="mt-n2 mb-4 d-flex flex-row form-group">
+					<input style={{width: "40vw"}}
 						className="form-control"
 						type="text"
+						name="tutorPhone"
+						placeholder="Teléfono del tutor"
+						onChange={this.onChangeHandler}
+					/>
+					<input style={{width: "40vw"}}
+						className="form-control"
+						type="text"
+						name="tutorEmail"
+						placeholder="Email del tutor"
+						onChange={this.onChangeHandler}
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						className="mt-4 form-control"
+						type="text"
 						name="strengths"
-						placeholder="Fortalezas..."
+						placeholder="Fortalezas del alumno..."
 						onChange={this.onChangeHandler}
 					/>
 				</div>
@@ -119,7 +149,7 @@ export class CreateStudentForm extends Component {
 						className="form-control"
 						type="text"
 						name="weakness"
-						placeholder="Aspectos que requieren atención..."
+						placeholder="Aspectos del alumno que requieren atención..."
 						onChange={this.onChangeHandler}
 					/>
 				</div>
@@ -128,15 +158,33 @@ export class CreateStudentForm extends Component {
 						className="form-control"
 						type="text"
 						name="motivations"
-						placeholder="Motivaciones..."
+						placeholder="Motivaciones del alumno..."
 						onChange={this.onChangeHandler}
 					/>
+				</div>
+				<div class="form-group">
+    			<label style={{fontSize: "1.7em"}} htmlFor="nivelEducativo">Nivel educativo</label>
+    			<select className="form-control" id="nivelEducativo">
+					<option>1er grado</option>
+					<option>2do grado</option>
+					<option>3er grado</option>
+					<option>4to grado</option>
+					<option>5to grado</option>
+					</select>
 				</div>
 				<h3 className="text-center d-block mb-3">Materias que necesita cursar</h3>
 				<div style={{width: "80vw"}} className="ml-auto mr-auto d-flex flex-wrap form-check form-check-inline">
                     { Array.isArray(this.props.subjects)&& this.props.subjects.length > 0 ? this.props.subjects.map( subject => {
                         return (
                         <SubjectCheckbox key = {subject.id} initialState={false} subject={subject} onChange={this.onCheckboxClicked} required/>
+                        )}
+                    ) : null}
+                </div>
+				<h3 className="text-center d-block mt-3 mb-3">Materias para las que tiene facilidad</h3>
+				<div style={{width: "80vw"}} className="ml-auto mr-auto d-flex flex-wrap form-check form-check-inline">
+                    { Array.isArray(this.state.subjectsId)&& this.state.subjectsId.length > 0 ? this.state.subjectsId.map( subject => {
+                        return (
+                        <StrengthCheckbox key = {subject.id+'strength'} initialState={false} subject={this.props.subjects.find(e => e.id === subject)} onChange={this.onStrengthCheckboxClicked} required/>
                         )}
                     ) : null}
                 </div>
