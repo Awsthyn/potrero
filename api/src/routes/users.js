224b3c2042bcc05f256b3b316ffb0043e760d963
nanacuna <nanacuna1992@gmail.com>
2020-09-16 12:29:45 -0300

    CV listo.

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,10 +1,14 @@
 const server = require('express').Router();
 
 // TRAEMOS LOS USUARIOS DE LA BASE DE DATOS
-const { User } = require('../db.js');
+const {
+    User
+} = require('../db.js');
 
 // TRAEMOS LOS SUBJECTS DE LA BASE DE DATOS
-const { Subject } = require('../db.js');
+const {
+    Subject
+} = require('../db.js');
 
 // TRAEMOS SEQUELIZE
 const Sequelize = require('sequelize');
@@ -14,100 +18,105 @@ const multer = require('multer');
 
 // Usamos esta funcion para guardar el archivo con extension
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         cb(null, `${__dirname}/uploads`);
-     },
-     filename: function (req, file, cb) {
+    },
+    filename: function (req, file, cb) {
         let ext = file.originalname.split('.').pop();
-        cb(null, file.fieldname + '-' + Date.now()+ '.' + ext);
-      }
+        cb(null, file.fieldname + '-' + Date.now() + '.' + ext);
+    }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+    storage: storage
+});
 
 server.get('/', (req, res) => {
     User.findAll()
-    .then( users => {
-        // OPERADOR TERNARIO, QUE SE FIJA SI EL ARRAY DE OBJETOS "USERS" ESTÁ VACÍO. ASÍ
-        // RESPONDE CON UN MENSAJE, O SINO, DEVUELVE EL ARRAY CON LOS USUARIOS DENTRO.
-        !users.length ? res.send("No hay usuarios disponibles.") : res.send(users)
-    })
-    .catch( err => {
-        res.send(err)
-    })
+        .then(users => {
+            // OPERADOR TERNARIO, QUE SE FIJA SI EL ARRAY DE OBJETOS "USERS" ESTÁ VACÍO. ASÍ
+            // RESPONDE CON UN MENSAJE, O SINO, DEVUELVE EL ARRAY CON LOS USUARIOS DENTRO.
+            !users.length ? res.send("No hay usuarios disponibles.") : res.send(users)
+        })
+        .catch(err => {
+            res.send(err)
+        })
 
 })
 
 // CREA UN USUARIO
-server.post('/', upload.single('cv'), ( req, res ) => {
-// RECIBE LOS DATOS DEL USUARIO POR BODY
+server.post('/', upload.single('cv'), (req, res) => {
+    // RECIBE LOS DATOS DEL USUARIO POR BODY
     let usuario;
     if (!req.file) {
         usuario = req.body
     } else {
-        usuario = { ...req.body, cv: `${req.file.filename}`}
+        usuario = {
+            ...req.body,
+            cv: `${req.file.filename}`
+        }
     }
     User.createInstanceFromBody(usuario)
-    .then( userCreated => {
-        res.send( userCreated )
-    })
-    .catch( err => { 
-        // SI HAY UN ERROR, LO DEVUELVE. POR SI FALTÓ UN CAMPO POR COMPLETAR O MISMO LOS DATOS NO SON VÁLIDOS.         
-        res.send( err )
-    })
+        .then(userCreated => {
+            res.send(userCreated)
+        })
+        .catch(err => {
+            // SI HAY UN ERROR, LO DEVUELVE. POR SI FALTÓ UN CAMPO POR COMPLETAR O MISMO LOS DATOS NO SON VÁLIDOS.         
+            res.send(err)
+        })
 })
 
 // BUSCA UN USUARIO EN ESPECÍFICO Y MUESTRA SUS DATOS.
-server.get('/:id', ( req, res ) => {
+server.get('/:id', (req, res) => {
     // ACÁ BUSCA UN USUARIO EN LA BASE DE DATOS
     User.findOne({
-        where:{
-            id: req.params.id
-        }
-    })
-    .then( userFound => {       
-        // SI ENCUENTRA AL USUARIO, LO ENVÍA. SINO, ENVÍA UN MENSAJE DE ERROR.
-        !userFound ? res.send("El usuario no existe.") : res.send( userFound ) 
-    })
-    .catch( err => {
-        res.send( err )
-    })
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(userFound => {
+            // SI ENCUENTRA AL USUARIO, LO ENVÍA. SINO, ENVÍA UN MENSAJE DE ERROR.
+            !userFound ? res.send("El usuario no existe.") : res.send(userFound)
+        })
+        .catch(err => {
+            res.send(err)
+        })
 })
 
 // BUSCA UN USUARIO Y MODIFICA LA INFORMACIÓN QUE LE HAYAN ENVIADO POR BODY
-server.put('/:id', ( req, res ) => {
-    if(req.body.disabled) {
+server.put('/:id', (req, res) => {
+    if (req.body.disabled) {
         User.findByPk(req.params.id)
-    .then(user => {
-        user.state = 'rechazado';
-        user.save();
-        res.json(user); 
-    })
+            .then(user => {
+                user.state = 'rechazado';
+                user.save();
+                res.json(user);
+            })
     }
     // BUSCA Y MODIFICA AL USUARIO ENCONTRADO.
     User.update(req.body, {
-        where: {
-            id: req.params.id
-        }
-    })
+            where: {
+                id: req.params.id
+            }
+        })
 
-    // UNA VEZ HECHO LOS CAMBIOS, ENVÍA SUS DATOS CON LA ACTUALIZACIÓN QUE HAYA REALIZADO.
-    .then(() => {
-       User.findOne({
-           where: {
-               id: req.params.id
-           }
-       })
-       .then( userWithChanges => {
-            res.send( userWithChanges )
-       })
-       .catch( err => {
-        res.send( err )
-    })
-    })
-    .catch( err => {
-        res.send( err )
-    })
+        // UNA VEZ HECHO LOS CAMBIOS, ENVÍA SUS DATOS CON LA ACTUALIZACIÓN QUE HAYA REALIZADO.
+        .then(() => {
+            User.findOne({
+                    where: {
+                        id: req.params.id
+                    }
+                })
+                .then(userWithChanges => {
+                    res.send(userWithChanges)
+                })
+                .catch(err => {
+                    res.send(err)
+                })
+        })
+        .catch(err => {
+            res.send(err)
+        })
 })
 
 // RELACIONA LAS MATERIAS CON USUARIOS 
@@ -115,24 +124,24 @@ server.post("/:id/subjects", (req, res) => {
     var id = req.params.id;
     var materias = req.body.materias;
     var user = User.findByPk(id);
-    
-    materias.map((m,i) => {
-    var subject = Subject.findOne({
-        where: {
-            name: m,
-        }
+
+    materias.map((m, i) => {
+        var subject = Subject.findOne({
+            where: {
+                name: m,
+            }
+        })
+        Promise.all([user, subject])
+            .then(values => {
+                var user = values[0];
+                var subject = values[1];
+                user.addSubject(subject);
+                if (i === arrayMateria.length - 1) res.send(user)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     })
-    Promise.all([user, subject])
-    .then(values => {
-        var user = values[0];
-        var subject = values[1];
-        user.addSubject(subject);
-        if(i === arrayMateria.length - 1) res.send(user)
-    })
-    .catch(err => {
-        res.send(err)
-    })
-    })  
 })
 
 
