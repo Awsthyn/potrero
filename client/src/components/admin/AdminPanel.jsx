2d2b8373b2dfd8c7fd755191017de0e403a46377
elefantenegro <elefantenegro64@gmail.com>
2020-09-17 14:56:03 -0300

    Lucca <3

--- client/src/components/admin/AdminPanel.jsx
+++ client/src/components/admin/AdminPanel.jsx
@@ -21,18 +21,7 @@ export default () => {
     const classes = useStyles()
              return    (
                  <div className={classes.root}>
-                    <div className="jumbotron">
-                        <h1 className="display-4">Panel de ADMIN</h1>
-                        <p className="lead">Este panel está en construcción</p>
-                        <hr className="my-4"/>
-                        <p>De todas maneras, podemos encontrar funcionando la administración pertinente a:</p>
-                        <div className="container-fluid">
-                            <Link to='/admin/estudiantes' type="button" className="ml-3 btn btn-primary border"> Alumnos </Link>
-                            <Link to='/admin/voluntarios/' type="button" className="ml-3 btn btn-primary border"> Voluntarios/Postulantes </Link>
-                            <Link to='/admin/usuarios' type="button" className="ml-3 btn btn-warning border"> Docentes </Link>
-                            <Link to='/admin/materias' type="button" className="ml-3 btn btn-danger border"> Materias </Link>
-                        </div>
-                    </div>
+                    
                 </div>
                 )
         }
