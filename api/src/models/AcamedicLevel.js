cecdffeac9420a74bde9d9d1384a3f2bfae50728
ioogustavo <gustavoalt93@gmail.com>
2020-09-17 23:37:18 -0300

    Agregado relacion AcademicLevel-Education-Level con sus rutas creadas, faltan algunos includes en GET

--- api/src/models/AcamedicLevel.js
+++ api/src/models/AcamedicLevel.js
@@ -1,27 +1,27 @@
 const Sequelize = require("sequelize");
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    const AcademicLevel = sequelize.define('academicLevel', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: new Date()
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: new Date()
-        },
+  const AcademicLevel = sequelize.define("academicLevel", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+  });
+  AcademicLevel.createInstanceFromBody = function ({ name, educationLevelId }) {
+    return AcademicLevel.create({
+      name,
+      educationLevelId,
     });
-    AcademicLevel.createInstanceFromBody = function ({ name, educationLevelId}) {
-        return AcademicLevel.create({
-            name,
-            educationLevelId
-        });
-    };
+  };
 };
