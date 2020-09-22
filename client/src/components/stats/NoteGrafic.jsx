9eb552ccf66b7e368789b43b72e41d973be21b46
Elena Note <elebeatrizgonzalez@gmail.com>
2020-09-22 16:55:13 -0300

    Grafico de barras

--- client/src/components/stats/NoteGrafic.jsx
+++ client/src/components/stats/NoteGrafic.jsx
@@ -1,97 +1,101 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Bar, Line, Pie } from "react-chartjs-2";
-import * as actions from "../../redux/actions/stats.js";
+import { useSelector, useDispatch } from 'react-redux';
+import { Pie, Line, Bar} from 'react-chartjs-2';
+import * as actions from '../../redux/actions/stats.js';
+import React from 'react';
 
-class NoteGrafic extends React.Component {
-  state = {
-    respuesta: [],
-    dataAssistance: [],
-    dataInassistance: [],
-    dataDelay: [],
-    datos: [],
-    opciones: [],
-  };
+class NoteGrafic extends React.Component{
+    state={
+        respuesta:[],
+        dataAssistance:[],
+        dataInassistance:[],
+        dataDelay:[],
+        datos:[],
+        opciones:[],
+    }
 
-  async peticion() {
-    var peticion = await fetch("http://localhost:3001/stats/qualification");
-    var respuesta = await peticion.json();
-    console.log("respuesta", respuesta);
-    var dataAssistance = [];
-    var dataInassistance = [];
-    var dataDelay = [];
-    this.setState({
-      respuesta: respuesta,
-    });
-    dataAssistance.push(respuesta.assistance);
-    dataInassistance.push(respuesta.inassistance);
-    dataDelay.push(respuesta.delay);
+    async peticion(){
+        var peticion= await fetch('http://localhost:3001/stats/assistances');
+        var respuesta= await peticion.json();
+        console.log(respuesta)
+        var dataAssistance=[];
+        var dataInassistance=[];
+        var dataDelay=[];
+        this.setState({
+            respuesta: respuesta
+        })
 
-    this.setState({
-      respuesta: respuesta,
-      dataAssistance: dataAssistance,
-      dataInassistance: dataInassistance,
-      dataDelay: dataDelay,
-    });
-    // console.log("Soy dataAssistance",this.state.dataAssistance)
-    // console.log("Soy dataInassistance",this.state.dataInassistance)
-    // console.log("Soy delay",this.state.dataDelay)
-  }
+            dataAssistance.push(respuesta.assistance);
+            dataInassistance.push(respuesta.inassistance);
+            dataDelay.push(respuesta.delay);
 
-  getChartData() {
-    console.log(this.state);
-    var arreglo = [];
-    this.state.respuesta.map(elem => {
-      arreglo.push(elem)
-    })
+        this.setState({
+            respuesta: respuesta,
+            dataAssistance: dataAssistance,
+            dataInassistance: dataInassistance,
+            dataDelay: dataDelay,
+        })
+    }
 
+  getChartData(){
+          const datos={
+              //hacer map para labels
+              labels: ['Oferta ','Demanda', 'Otra cosa'],
+              datasets:[
+                  {
+                      label: 'Asistencias totales',
+                      //map para data
+                     /* data: [this.state.dataAssistance, this.state.dataInassistance, this.state.dataDelay],*/
+                     data:[50,100,200],
+                      fill: false,
+                      backgroundColor:[
+                          'rgba(140, 198, 62, 0.6)',
+                          'rgba(54, 162 , 235, 0.6)',
+                          'rgba(73, 43, 196, 0.6)',
+                                        ]
+                  }
+              ]}
+              console.log("Soy datos" ,datos)
+               const opciones= {
+                   responsive: true,
+                   maintainAspectRatio: false,
+                   displayTitle: true,
+                   displayLegend: true,
+                   legendPosition: 'bottom',
+                   location: 'Asistencias totales',
+               }
+               this.setState({
+                   datos: datos,
+                   opciones: opciones,
+               })
+        }
 
-    const datos = {
-      labels: arreglo,
-      datasets: [
-        {
-          label: "Meses totales",
-          data: this.state.respuesta
-          ,
-          fill: true,
-          backgroundColor: "rgba(54, 162 , 235, 0.6)",
-          borderColor: "rgba(54, 162 , 235, 0.6)",
-          pointBorderColor: "rgba(54, 162 , 235, 0.6)",
-        },
-      ],
-    };
-    const opciones = {
-      responsive: true,
-      maintainAspectRatio: false,
-      displayTitle: true,
-      displayLegend: true,
-      legendPosition: "bottom",
-      location: "Asistencias totales",
-    };
-    this.setState({
-      datos: datos,
-      opciones: opciones,
-    });
-  }
+        async componentDidMount(){
+            await this.peticion();
+            await this.getChartData();
+        }
 
-  async componentDidMount() {
-    await this.peticion();
-    await this.getChartData();
-  }
+     render(){
+      return (
+        <div>
+              <Bar
+              type
+              data = {this.state.datos}
+              options ={{
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+           yAxes: [{
+               ticks: {
+                   suggestedMin: 50,
+                   suggestedMax: 100
+               }
+           }]
+       }
 
-  render() {
-    return (
-      <div>
-        <Line
-          type
-          data={this.state.datos}
-          options={{
-            responsive: true,
-            maintainAspectRatio: false,
-          }}
-        ></Line>
-      </div>
-    );
+                  }}>
+                 </Bar>
+          </div>
+      )
   }
 }
 
