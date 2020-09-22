f5cf8b224481add96bcc48a8761692485572d3d3
elefantenegro <elefantenegro64@gmail.com>
2020-09-22 10:03:35 -0300

    Step2

--- client/src/components/admin/AdminNavBar.jsx
+++ client/src/components/admin/AdminNavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Drawer, List,ListItem,ListItemIcon,ListItemText} from '@material-ui/core';
 import {makeStyles} from '@material-ui/styles';
 import HomeIcon from '@material-ui/icons/Home';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import { connect } from 'react-redux';
+import PieChartIcon from '@material-ui/icons/PieChart'
 import { sessionLogout } from '../../redux/actions/session';
 
 
@@ -23,19 +24,21 @@ const useStyles = makeStyles({
     font:{
         maxWidth:'auto',
         fontFamily: 'Poppins',
-        margin:5,
+        margin:3,
+        color:'white',
     },
     Navicons:{
         display:'flex',
         flex: 1,
         justifyContent:'flex-end',
-        alignItems:'center',
+        alignItems:'center'
     },
     profileBox:{
         maxWidth:'auto',
         justifyContent:'flex-end',
     },
     link:{
+        flexDirection:'row',
         textDecoration:'none',
         color:'gray',
         '&:hover': {
@@ -43,6 +46,20 @@ const useStyles = makeStyles({
             textDecoration:'none',
          },
     },
+    icons:{
+        maxWidth:30,
+        maxHeight:30,
+    },
+    toolbar:{
+        display:'flex',
+    },
+    grouplinks:{
+        display:'row',
+        color:'white',
+        position:'flex-end',
+        alignItems:'center',
+        width:200
+    }
 
 })
 
@@ -55,27 +72,32 @@ const AdminNavBar = (props) => {
     return(
         <div className={classes.root}>
                 <AppBar className={classes.root}>
-                    <Toolbar>
-                    <Typography className={classes.font} variant='h6'>
-                            El Potrero / Panel de Admin
-                     </Typography>
-                    <List className={classes.Navicons}>
-                            <ListItem className={classes.profileBox} button>
-                                <Typography className={classes.font} variant='h6'>
-                                    {`${props.session.firstName} ${props.session.lastName}`}
-                                </Typography>
-                                <ListItemIcon >
-                                    <AccountBoxIcon/> 
-                                </ListItemIcon>           
-                                    </ListItem>
-
-                            <Link to= '/' className={classes.link}>
-                                    <ListItem onClick={handleLogout} button>
+                    <Toolbar >
+                    <ListItem button>
+                        <Typography  variant='h6' >
+                            <ListItemIcon >
+                                    Panel <PieChartIcon /> 
+                            </ListItemIcon>   
+                        </Typography>
+                     </ListItem>
+                    <List className={classes.grouplinks} >
+                            <ListItem className={classes.icon} button>
+                                <Typography className={classes.font} variant='h6' >
                                         <ListItemIcon >
-                                                <PowerSettingsNewIcon/> 
-                                        </ListItemIcon>           
-                                    </ListItem>
-                            </Link>
+                                            {`${props.session.firstName} ${props.session.lastName}`} <AccountBoxIcon className={classes.icon} />  
+                                                <ListItem onClick={handleLogout} className={classes.icon} button>
+                                                    <ListItemIcon >
+                                                            <PowerSettingsNewIcon className={classes.icon}/>
+                                                    </ListItemIcon>           
+                                                </ListItem> 
+                                        </ListItemIcon>  
+                                        <Link to= '/' className={classes.link}>
+                                        
+                                         </Link>
+                                </Typography>
+                            </ListItem>
+
+                            
                                   
                           
                         </List>
