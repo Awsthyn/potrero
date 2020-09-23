b7d824e578a1d1c1bee0697a7d19c44fe7e921ed
MarcosLeonelMantay <marcosleonelmantay@gmail.com>
2020-09-23 13:36:50 -0300

    commit

--- client/src/components/formularioVoluntario/VoluntarioForm.jsx
+++ client/src/components/formularioVoluntario/VoluntarioForm.jsx
@@ -111,12 +111,11 @@ export default class VolunteerForm extends React.Component {
 					!this.state.info.phone 
 						? (control = true)
 						: false}
-						<div style={{position: 'absolute', left: '63%', bottom: '70px'}} > 
+						<div> 
 							<svg viewBox="0 0 16 16" className={styles.leftArrow} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 							<path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
 							</svg> 
-						</div>
-					<Button
+							<Button
 						disabled={control ? true : false}
 						variant="contained"
 						className={styles.testButton}
@@ -127,6 +126,8 @@ export default class VolunteerForm extends React.Component {
 						Continuar
 						<span style={{margin:"10px"}} className="material-icons">arrow_forward</span>
 					</Button>
+						</div>
+					
 				</form>
 			</div>
 			</div>
