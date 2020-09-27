94cf3676ad5432cddf2958560baa5c385f90917e
Elena Note <elebeatrizgonzalez@gmail.com>
2020-09-27 13:09:23 -0300

    envio de mail al asesor

---
+++ api/src/routes/mailAdvisor.js
@@ -0,0 +1,37 @@
+const server = require("express").Router();
+const nodemailer = require("nodemailer");
+const {User} = require("../db.js");
+
+//Ver como viene el texto desde un post
+server.post('/advisor', function(req, res){
+    console.log("Body del emal: ", req.body.body)
+    User.findOne({where: {email : req.body.email}})
+    .then(volunteer =>{
+        const transporter= nodemailer.createTransport({
+            service: "gmail",
+            auth:{
+                user: 'fundacionelpotrero1@gmail.com',
+                pass: 'Henryelpotrero'
+            }
+        });
+
+        var mailOptions = {
+            from: "El Potrero",
+            to: volunteer.email,
+            subject: 'Fundación El Potrero',
+            text: req.body.body
+        }
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    res.status(500).send(error.message)
+                } else {
+                    console.log("reset enviado");
+                    res.status(200).jsonp(req.body)
+                }
+            })
+        })
+        .catch(err => console.log(err));
+    })
+
+    module.exports = server;
