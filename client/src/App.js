f91188b51811fb2283a7d469bcc05e704b37b8a9
Elena Note <elebeatrizgonzalez@gmail.com>
2020-09-27 13:08:47 -0300

    ruta form asesor

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import StudentCard from './components/student/StudentCard/StudentCard';
 import AsesorProfile from './components/AsesorProfile/AsesorProfile';
 import ClassAssignation from "./components/student/classes/ClassAssignation"
 import SubjectsPerStudent from "./components/student/classes/SubjectsPerStudent"
+import AdvisorFormMail from './components/admin/AdvisorFormMail';
 
 class App extends React.Component {
 
@@ -74,6 +75,8 @@ class App extends React.Component {
         <Route exact path='/admin/estudiantes/listadematerias/:id' component={SubjectsPerStudent} />
 
 
+        <Route exact path='/admin/advisorFormMail' component={AdvisorFormMail} />
+
 
         <Route exact path='/usuario/login' component={Login} />
         <Route exact path='/usuario/perfil' component={MiPerfil} />
@@ -81,7 +84,7 @@ class App extends React.Component {
 
         <Route exact path='/login/:token' component={ResetPassword} />
 
-       
+
 
 		<Route
         path="/studentCard"
