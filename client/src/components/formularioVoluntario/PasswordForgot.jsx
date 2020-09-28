d7654e37a34369ce1e03573490c0cc1977f3acad
luccalipisky <luccalipisky@gmail.com>
2020-09-28 12:30:24 -0300

    estilos en forgotPassword

--- client/src/components/formularioVoluntario/PasswordForgot.jsx
+++ client/src/components/formularioVoluntario/PasswordForgot.jsx
@@ -3,6 +3,8 @@ import { withRouter } from "react-router-dom";
 import { getCurrentUser, changePassword, sessionLogin} from "../../redux/actions/session"
 import { connect } from "react-redux";
 import swal from 'sweetalert';
+import style from './PasswordForgot.module.css'
+import logo from '../VolunteerFormAssets/logo.png';
 
 
 class ResetPassword extends React.Component {
@@ -61,18 +63,36 @@ class ResetPassword extends React.Component {
     render() {
         return (
 
-        <div className="d-flex border border-secondary m-auto m-0 shadow p-3 mb-5 bg-white rounded m-50 d-flex flex-column align-items-center" >
-            <form onSubmit={this.handleSubmit} className="form-group ">
-                <div className="form-group">
-                    <label className="lead">Nueva contraseña:</label>
-                    <input type="password" id="password" name="password" onChange={this.handleChange} className="form-control" value={this.state.password} placeholder={'Nueva contraseña'}/>
-                </div>
-                <div className="form-group">
-                    <label className="lead">Confirmar contraseña:</label>
-                    <input type="password" id="repeatPassword" name="repeatPassword" onChange={this.handleChange} className="form-control" value={this.state.repeatPassword} placeholder={'Confirmar contraseña'}/>
-                </div>
+        <div className = {style.container}>
 
-            <button type="submit" className="btn btn-dark lead" >Enviar</button>
+             <svg onClick={()=> this.props.history.push('/usuario/login')} viewBox="0 0 16 16" class={style.leftArrow} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                </svg>
+              
+
+            <form onSubmit={this.handleSubmit} className={style.formContainer}>
+                    <img className={style.logo} src={logo} alt="" />
+            
+                <div className={style.form}>
+                    
+                <div>
+                <h2 className={`${style.title}`}>Por último...</h2>
+                <label className={style.label}>Necesitamos que ingreses una nueva contraseña para restaurar tu cuenta. </label>
+                </div>
+                <div className = {style.inputContainer}>
+                    <input type="password" id="password" name="password" onChange={this.handleChange} className={style.input} value={this.state.password} placeholder={'Nueva contraseña'}/>
+                    <input type="password" id="repeatPassword" name="repeatPassword" onChange={this.handleChange} className={style.input} value={this.state.repeatPassword} placeholder={'Confirmar contraseña'}/>
+                </div>
+                    <button className={style.button} type="submit">Enviar
+                                    <svg viewBox="0 0 16 16" className={style.rightArrow} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                </svg>
+                            </button>
+                </div>
+               
+                    
+              
+
             </form>
         </div>
         )};
