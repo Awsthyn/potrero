fe4398fd7ef9665f66f34b11365a8521e312ffa9
ioogustavo <gustavoalt93@gmail.com>
2020-10-01 20:07:54 -0300

    mas bugs

--- client/src/components/stats/printDetail/OfWithDemDetail.jsx
+++ client/src/components/stats/printDetail/OfWithDemDetail.jsx
@@ -25,54 +25,67 @@ class OfWithDemDetail extends React.Component {
       offers: respuesta.allOffer,
     });
   }
-  
-  
-  async transformar(){
-    let subjD = [];
+
+  async transformar() {
+    let materiasDemanda = [];
+    let materiasOferta = [];
+
     for (let i = 0; i < this.state.demand.length; i++) {
       for (const property in this.state.demand[i]) {
-        subjD.push(property)
+        materiasDemanda.push(property);
       }
     }
-    this.setState({
-      listaDemanda: subjD,
-    })
+
+    for (let i = 0; i < this.state.offers.length; i++) {
+      for (const property in this.state.offers[i]) {
+        materiasOferta.push(property);
+      }
     }
 
+    this.setState({
+      listaDemanda: materiasDemanda,
+      listaOferta: materiasOferta,
+    });
+  }
 
   async componentDidMount() {
     await this.peticion();
     await this.transformar();
+    this.setState({
+      demand: this.props.location.demandas,
+      offers: this.props.location.ofertas,
+    });
+    console.log(this.props.location);
   }
 
   render() {
-
     let testeando = {
       testing: [],
       claramente: [],
       probando: [],
-    }
-
-    this.state.demand.forEach( demandante => {
-      this.state.listaDemanda.forEach( element => {
-        if(demandante[element]){
-          testeando.claramente.push({[element] : demandante[element]})
-          testeando.probando.push(element)
-            testeando.testing = testeando.testing.concat(demandante[element])
+    };
+    if (this.state.demand) {
+      this.state.demand.forEach((demandante) => {
+        this.state.listaDemanda.forEach((element) => {
+          if (demandante[element]) {
+            testeando.claramente.push({ [element]: demandante[element] });
+            testeando.probando.push(element);
+            testeando.testing = testeando.testing.concat(demandante[element]);
           }
-        })
-     })
-     let recorrer = [];
+        });
+      });
+    }
+    let recorrer = [];
     return (
       <div className="detailAssist">
+        {console.log(this.state)}
         <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+        <br>
         <div
           className={`titlePotrero ocultoimpresion row d-flex justify-content-center`}
         >
           <h1>Fundación El Potrero</h1>
+          <img src="https://static.wixstatic.com/media/a54840_a2385331f0da4e698b63580c4db7ef02%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/a54840_a2385331f0da4e698b63580c4db7ef02%7Emv2.png" />
         </div>
         <button
           onClick={() => window.print()}
@@ -80,7 +93,6 @@ class OfWithDemDetail extends React.Component {
         >
           Imprimir
         </button>
-        {this.state.info && this.armarDatos()}
         <br />
         <br />
         <br />
@@ -88,53 +100,47 @@ class OfWithDemDetail extends React.Component {
         <br />
         <br />
         <br />
-        
-       { 
-       this.state.demand.length > 0 ?
-        (<div>
 
-        
-        <h1>Demanda de materias</h1>
-        <table className="table">
-          <thead className="thead-dark">
-            <tr>
-            <th scope="col">Estado</th>
-              <th scope="col">Materias</th>
-              <th scope="col">Nombre</th>
-              <th scope="col">Apellido</th>
-            </tr>
-          </thead>
-          <tbody>
-           {
-             
-             testeando.claramente.map( ola => {
-               for (const key in ola) {
-                 if (ola.hasOwnProperty(key)) {
-                   const element = ola[key];
-                   element.map( cadaDemandador => {
-                    recorrer.push(cadaDemandador)
-                  })
+        {/* {this.state.demand.length > 0 ? (
+          <div>
+            <h1>Demanda de materias</h1>
+            <table className="table">
+              <thead className="thead-dark">
+                <tr>
+                  <th scope="col">Estado</th>
+                  <th scope="col">Materias</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Apellido</th>
+                </tr>
+              </thead>
+              <tbody>
+                {testeando.claramente.map((ola) => {
+                  for (const key in ola) {
+                    if (ola.hasOwnProperty(key)) {
+                      const element = ola[key];
+                      element.map((cadaDemandador) => {
+                        recorrer.push(cadaDemandador);
+                      });
 
-                  return (<tr>
-                <th scope="row">Demanda</th>
-                <td>{key}</td>
-                <td>Franco</td>
-                <td>Matus</td>
-                </tr>)
-                 }
-               }
-          })
-        }
-
-        </tbody>
-        </table>
-        </div>)
-      : (
-        <div>
-          <h3>No hay materias disponibles.</h3>
-        </div>
-      )  
-      } 
+                      return (
+                        <tr>
+                          <th scope="row">Demanda</th>
+                          <td>{key}</td>
+                          <td>Franco</td>
+                          <td>Matus</td>
+                        </tr>
+                      );
+                    }
+                  }
+                })}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div>
+            <h3>No hay materias disponibles.</h3>
+          </div>
+        )} */}
       </div>
     );
   }
