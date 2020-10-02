686696ed1ae4d8b806a522b252404df183b93dc7
nanacuna <nanacuna1992@gmail.com>
2020-10-02 02:33:25 -0300

    Cambios

--- client/src/components/admin/DetalleHorariosVoluntario.jsx
+++ client/src/components/admin/DetalleHorariosVoluntario.jsx
@@ -219,9 +219,6 @@ function DetalleHorariosVoluntario( { id, schedule, getUserSchedule } ) {
                 :
                 <div><h4 className={style.title}>Rango no disponible</h4></div> 
             }
-            <svg viewBox="0 0 16 16" class={style.leftArrow} onClick={()=> window.history.go(-1)} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
-            </svg>
         </div>
     )
 }
